52923fbb96dce5c7bae843819d4b08815579675f
Aliaksandr Yakubouski <stockupby@gmail.com>
2024-10-28 11:15:03 +0100

    Add basic resize feature

--- src/fitcropedit.js
+++ src/fitcropedit.js
@@ -1,1 +1,22 @@
-console.log('fitcropedit');
+
+import { initParams } from "./config";
+
+console.log("fitcropedit.js");
+
+const { canvasMultiplier } = initParams;
+
+const container = document.getElementById("fitcropedit");
+
+const canvas = document.getElementById("fc-canvas");
+const ctx = canvas.getContext("2d");
+
+const resizeObserver = new ResizeObserver((entries) => {
+  const containerWidth = entries[0].target.clientWidth;
+  const containeHeight = entries[0].target.clientHeight;
+  canvas.width = containerWidth * canvasMultiplier;
+  canvas.height = containeHeight * canvasMultiplier;
+  ctx.fillStyle = "green";
+  ctx.fillRect(10, 10, 150, 100);
+});
+
+resizeObserver.observe(container);
